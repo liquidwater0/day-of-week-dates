d3c755eee584766ec7c6b1ba9f94f7a1cc3e8bdd
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-02-22 20:26:42 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,101 @@
+import { useState } from 'react';
+import "./scss/App.scss";
+
+//Fix dates on date inputs being 1 day off
+
+function App() {
+	const [dates, setDates] = useState<string[]>([]);
+	const [startDate, setStartDate] = useState<Date>(() => {
+		const now = new Date();
+		return new Date(now.setMonth(now.getMonth() - 1));
+	});
+	const [endDate, setEndDate] = useState<Date>(new Date());
+	const [dayOfWeek, setDayOfWeek] = useState<string>("sunday");
+	const [displayedDayOfWeek, setDisplayedDayOfWeek] = useState<string>(dayOfWeek);
+
+	function getDatesForDayOfWeek() {
+		const daysOfWeeks = new Map([["sunday", 0], ["monday", 1], ["tuesday", 2], ["wednesday", 3], ["thursday", 4], ["friday", 5], ["saturday", 6]]);
+		const datesArray: string[] = [];
+		let date: Date = new Date(startDate);
+		
+		while (date.getTime() <= endDate.getTime()) {
+			if (date.getDay() === daysOfWeeks.get(dayOfWeek)) {
+				datesArray.push(`${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`);
+			}
+
+			date = new Date(date.setDate(date.getDate() + 1));
+		}
+
+		setDates(datesArray);
+		setDisplayedDayOfWeek(dayOfWeek);
+	}
+
+	function formatDate(date: Date) {
+		const month = (date.getMonth() + 1).toString().padStart(2, "0");
+		const day = date.getDate().toString().padStart(2, "0");
+		const year = date.getFullYear();
+
+		return `${year}-${month}-${day}`;
+	}
+
+	return (
+		<>
+			<header className='header'>
+				<div>
+					<label htmlFor="startDateInput">Start Date</label>
+					<input 
+						id='startDateInput'
+						type="date"
+						defaultValue={formatDate(startDate)}
+						onChange={({ target }) => setStartDate(new Date(target.value))}
+					/>
+				</div>
+				<div>
+					<label htmlFor="endDateInput">End Date</label>
+					<input 
+						id='endDateInput'
+						type="date"
+						defaultValue={formatDate(endDate)}
+						onChange={({ target }) => setEndDate(new Date(target.value))}
+					/>
+				</div>
+				<div>
+					<select 
+						defaultValue={dayOfWeek}
+						onChange={({ target }) => setDayOfWeek(target.value)} 
+					>
+						<option value="sunday">Sunday</option>
+						<option value="monday">Monday</option>
+						<option value="tuesday">Tuesday</option>
+						<option value="wednesday">Wednesday</option>
+						<option value="thursday">Thursday</option>
+						<option value="friday">Friday</option>
+						<option value="saturday">Saturday</option>
+					</select>
+				</div>
+			</header>
+
+			<main className='main'>
+				<h1>Dates on { displayedDayOfWeek }</h1>
+				<div className='dates-container container'>
+					{
+						dates.map(date => {
+							return <div className='date' key={date}>{ date }</div>
+						})
+					}
+				</div>
+			</main>
+
+			<footer className='footer'>
+				<button 
+					className='button'
+					onClick={getDatesForDayOfWeek}
+				>
+					Get Dates
+				</button>
+			</footer>
+    	</>
+  	);
+}
+
+export default App;
