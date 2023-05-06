54469f08e69a623b5d291f3f8398cd29d8d194e3
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-05-06 13:38:15 -0500

    date tooltip

--- src/components/DateButton.tsx
+++ src/components/DateButton.tsx
@@ -2,6 +2,7 @@ export default function DateButton({ date }: { date: string }) {
     return (
         <button 
             className='date-button' 
+            title={new Date(date).toDateString()}
             // onClick={() => console.log(date)}
         >
             { date }
